7c32ab4ba14c364d1534d02aa4cb936da0b8432e
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-06-10 13:46:35 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Review from './Review';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <h2 id="review-heading">Our Reviews</h2>
+        <Review />
+      </section>
+    </main>
+  );
+}
+
+export default App;
